3c7a06b1258203e0b82d5c3d2f06f0f7348e06ef
Wmugacha <wilfredmugacha@gmail.com>
2025-06-27 17:47:06 +0800

    first graphql query

---
+++ alx_backend_graphql_crm/schema.py
@@ -0,0 +1,11 @@
+import graphene
+from graphene_django.types import DjangoObjectType
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+
+    def resolve_hello(self, info):
+        return "Hello, GraphQL!"
+
+
+schema = graphene.Schema(query=Query)
--- alx_backend_graphql_crm/settings.py
+++ alx_backend_graphql_crm/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'graphene_django',
+    'django_filters',
+    'crm',
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'alx_backend_graphql_crm.schema.schema'
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
